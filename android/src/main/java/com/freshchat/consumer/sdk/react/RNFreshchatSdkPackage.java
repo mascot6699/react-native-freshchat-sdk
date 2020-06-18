a7906448f7c49eb18d2e69507d5cce68c7bf1933
mascot6699 <umangshucool@gmail.com>
2020-06-18 09:22:32 +0000

    Enables androidX and Jetifier

--- android/src/main/java/com/freshchat/consumer/sdk/react/RNFreshchatSdkPackage.java
+++ android/src/main/java/com/freshchat/consumer/sdk/react/RNFreshchatSdkPackage.java
@@ -1,6 +1,6 @@
 package com.freshchat.consumer.sdk.react;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
